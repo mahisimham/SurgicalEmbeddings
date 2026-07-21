c6c1f78c02a6ebeaa7bf2c60fe4ea35e394a3f22
mahisimham <mahithasimham@gmail.com>
2026-07-21 15:38:37 -0700

    updated example_usage

--- example_usage.py
+++ example_usage.py
@@ -3,13 +3,24 @@ from src.surgical_embeddings import (
     save_embeddings,
 )
 
+"""
+Parameter info:
+- text: List of strings to generate embeddings for
+ex: ["laparoscopic", "robotic"]
+- model_name: Model name(s) from HuggingFace to use for generating embeddings. 
+It can be a single model, a list of models, or "all" to use all available models
+ex: "all", "MiniLM", "SapBERT", "BGE_Large"
+- (optional) apply_pca: boolean flag to indicate whether to apply PCA transformation to the embeddings
+ex: True, False
+- (optional but required if apply_pca is True) variance_percent: The percentage of variance to retain if PCA is applied.
+ex: 95, 99, None (for full PCA)
+"""
+
+
 text = [
     "laparoscopic",
     "robotic",
 ]
-
-# model_name can be a single model, a list of models, or "all" to use all available models
-# "all", "MiniLM", "SapBERT", "BGE_Large"
 
 # Generate and save embeddings for all models, no PCA or variance applied
 all_embeddings = generate_embeddings(['laparoscopic', 'robotic'], model_name="all")
